9f8ad8498c58efe1b8cc7a8f5e71789dc709f2ab
gmello <esquilogp@gmail.com>
2015-02-23 16:33:14 +0000

    First Commit Script to make pci devices assignables to DomUs using the xen_pciback (PV) or pciback(HVM)

---
+++ pcipassthru_devs.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Verify if PCI IDs have been provided
+if [ $# -eq 0 ]; then
+    echo " Require PCI Devices in the format : domain:bus:slot.function"
+    exit 1
+fi
+
+# Verify if the xen_pciback(PV) or pciback(HVM) driver is not loaded
+# then load it
+[ ! -d /sys/bus/pci/drivers/pciback ] && \
+    sudo modprobe xen_pciback
+
+for pcidev in $@; do
+    if [ -h /sys/bus/pci/devices/"$pcidev"/driver ]; then
+
+        # For PCI devices with modules attached to them
+        sudo echo "Unbinding $pcidev from  $(basename $(readlink /sys/bus/pci/devices/"$pcidev"/driver))"
+        sudo echo -n "$pcidev" > /sys/bus/pci/devices/"$pcidev"/driver/unbid
+
+        sudo echo "Binding $pcidev to pciback"
+        sudo echo -n "$pcidev" > /sys/bus/pci/drivers/pciback/new_slot
+        sudo echo -n "$pcidev" > /sys/bus/pci/drivers/pciback/bind
+    else
+        #For PCI devices with no modules attached to them
+        sudo echo "Binding $pcidev to pciback"
+        sudo echo -n "$pcidev" > /sys/bus/pci/drivers/pciback/new_slot
+        sudo echo -n "$pcidev" > /sys/bus/pci/drivers/pciback/bind
+    fi
+done
+
+
