a761f072414ad0b6352149e144b7f6e4d49e3cd6
gmello <esquilogp@gmail.com>
2015-02-23 17:52:49 +0000

    remove sudo

--- pcipassthru_devs.sh
+++ pcipassthru_devs.sh
@@ -11,23 +11,23 @@ set -e
 # then load it
 [ ! -d /sys/bus/pci/drivers/pciback ] && \
     echo "loading xen_pciback"
-    sudo modprobe xen_pciback
+    modprobe xen_pciback
 
 for pcidev in $@; do
     if [ -h /sys/bus/pci/devices/"$pcidev"/driver ]; then
 
         # For PCI devices with modules attached to them
-        sudo echo "Unbinding $pcidev from  $(basename $(readlink /sys/bus/pci/devices/"$pcidev"/driver))"
-        sudo echo -n "$pcidev" > /sys/bus/pci/devices/"$pcidev"/driver/unbind
+        echo "Unbinding $pcidev from  $(basename $(readlink /sys/bus/pci/devices/"$pcidev"/driver))"
+        echo $pcidev > /sys/bus/pci/devices/"$pcidev"/driver/unbind
 
-        sudo echo "Binding $pcidev to pciback"
-        sudo echo -n "$pcidev" > /sys/bus/pci/drivers/pciback/new_slot
-        sudo echo -n "$pcidev" > /sys/bus/pci/drivers/pciback/bind
+        echo "Binding $pcidev to pciback"
+        echo $pcidev > /sys/bus/pci/drivers/pciback/new_slot
+        echo $pcidev > /sys/bus/pci/drivers/pciback/bind
     else
         #For PCI devices with no modules attached to them
-        sudo echo "Binding $pcidev to pciback"
-        sudo echo -n "$pcidev" > /sys/bus/pci/drivers/pciback/new_slot
-        sudo echo -n "$pcidev" > /sys/bus/pci/drivers/pciback/bind
+        echo "Binding $pcidev to pciback"
+        echo $pcidev > /sys/bus/pci/drivers/pciback/new_slot
+        echo $pcidev > /sys/bus/pci/drivers/pciback/bind
     fi
 done
 
