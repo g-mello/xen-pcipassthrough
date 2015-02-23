1534ae4dd14264e6c5c924cdb76568b6aee3efb4
gmello <esquilogp@gmail.com>
2015-02-23 17:17:56 +0000

    fixed unbind bug

--- pcipassthru_devs.sh
+++ pcipassthru_devs.sh
@@ -4,11 +4,13 @@
 if [ $# -eq 0 ]; then
     echo " Require PCI Devices in the format : domain:bus:slot.function"
     exit 1
-fi
+fi 
 
+set -e 
 # Verify if the xen_pciback(PV) or pciback(HVM) driver is not loaded
 # then load it
 [ ! -d /sys/bus/pci/drivers/pciback ] && \
+    echo "loading xen_pciback"
     sudo modprobe xen_pciback
 
 for pcidev in $@; do
@@ -16,7 +18,7 @@ for pcidev in $@; do
 
         # For PCI devices with modules attached to them
         sudo echo "Unbinding $pcidev from  $(basename $(readlink /sys/bus/pci/devices/"$pcidev"/driver))"
-        sudo echo -n "$pcidev" > /sys/bus/pci/devices/"$pcidev"/driver/unbid
+        sudo echo -n "$pcidev" > /sys/bus/pci/devices/"$pcidev"/driver/unbind
 
         sudo echo "Binding $pcidev to pciback"
         sudo echo -n "$pcidev" > /sys/bus/pci/drivers/pciback/new_slot
